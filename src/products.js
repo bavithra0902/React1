e1d3ed0317f6cdf5700c385b06e5f1719ab8a467
Muthamil R <muthamil1998@gmail.com>
2020-10-25 12:06:08 +0530

    First Commit

---
+++ src/products.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    _id: '1',
+    name: 'boAt Rockerz Bluetooth Headphones',
+    image: '/images/boAt-Rockerz.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'boAt',
+    category: 'Electronics',
+    price: 6000,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: '2',
+    name: 'Car-Toy',
+    image: '/images/car.jpg',
+    description:
+      'It can almost function as a Realistic car',
+    brand: 'Eveready',
+    category: 'Toys',
+    price: 750,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    _id: '3',
+    name: '64inches Samsung UHD TV',
+    image: '/images/download.jpg',
+    description:
+      'Ultra HD and Smart Tv. It can work as tv and Streaming platform',
+    brand: 'Samsung',
+    category: 'Electronics',
+    price: 19000,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: '4',
+    name: 'Iphone 12',
+    image: '/images/iphone12.jpg',
+    description:
+      'Its Iphone Nothing more to state',
+    brand: 'Iphone',
+    category: 'Electronics',
+    price: 74000,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    _id: '5',
+    name: 'Panda Black and White',
+    image: '/images/panda.jpg',
+    description:
+      'Fluffy cotton,washable and soft',
+    brand: 'Funskool',
+    category: 'Toys',
+    price: 250,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    _id: '6',
+    name: 'White Shirt',
+    image: '/images/shirt.jpg',
+    description:
+      'Comfortable,Fit and Handsome. Available in all Sizes',
+    brand: 'Peter England',
+    category: 'Fashion',
+    price: 450,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: '7',
+    name: 'Task Master Action Figure',
+    image: '/images/taskmaster.jpg',
+    description:
+      'Only prefered to kids above 8+ years. Movable arms,wrists,legs and head',
+    brand: 'LEGO',
+    category: 'Toys',
+    price: 300,
+    countInStock: 5,
+    rating: 4,
+    numReviews: 3,
+  },
+]
+
+export default products
